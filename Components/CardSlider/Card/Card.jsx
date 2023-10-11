19de8f15cca4fbd59ff9108db1bacdd768fb08ba
Shashank Raj <shashankraj3636@gmail.com>
2023-10-12 01:42:08 +0530

    done

--- Components/CardSlider/Card/Card.jsx
+++ Components/CardSlider/Card/Card.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import './Card.css'
+import React from "react";
+import "./Card.css";
 
 export default function Card({ name, abt, photo, link }) {
-    return (
-        <div className="card">
-            <div className="inner-card">
-                <a href={link} target="_blank" rel="noopener noreferrer">
-                    <img src={photo} alt={name} />
-                </a>
-                <h3>{name}</h3>
-                <h5>{abt}</h5>
-            </div>
-        </div>
-    )
+  return (
+    <div className="card">
+      <div className="inner-card">
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          <img src={photo} alt={name} />
+        </a>
+        <h3>{name}</h3>
+        <h5>{abt}</h5>
+      </div>
+    </div>
+  );
 }
