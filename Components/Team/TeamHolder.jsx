70d405b97348ed33b1b57ec6c2b2a9d944d1e1e7
Shashank Raj <shashankraj3636@gmail.com>
2023-10-12 14:25:51 +0530

    done

--- Components/Team/TeamHolder.jsx
+++ Components/Team/TeamHolder.jsx
@@ -11,53 +11,53 @@ export default function TeamHolder() {
           <TeamCard
             name="VryuShab Goyani"
             link="https://www.iiits.ac.in/home/administration/director/"
-            title="Director, IIIT Sri City, Chittoor"
+            title="Management Lead"
           />
         </div>
         <h2>Web Dev Team</h2>
         <div className="team_con">
           <TeamCard
             link="https://www.linkedin.com/in/nikhil-tripathi/"
-            name="Nikhil Tripathi"
-            title="Assistant Professor, IIIT Sri City"
+            name="Shashank Raj"
+            title="Web Dev Lead, IOTA"
           />
           <TeamCard
-            name="Dr. Neha Agrawal"
+            name="Shashank Raj"
             link="https://www.linkedin.com/in/dr-neha-agrawal-0a32776b/"
-            title="Assistant Professor, IIIT Sri City"
+            title="Web Dev Lead, IOTA"
           />
           <TeamCard
             link="https://www.linkedin.com/in/nikhil-tripathi/"
-            name="Nikhil Tripathi"
-            title="Assistant Professor, IIIT Sri City"
+            name="Shashank Raj"
+            title="Web Dev Lead, IOTA"
           />
           <TeamCard
-            name="Dr. Neha Agrawal"
+            name="Shashank Raj"
             link="https://www.linkedin.com/in/dr-neha-agrawal-0a32776b/"
-            title="Assistant Professor, IIIT Sri City"
+            title="Web Dev Lead, IOTA"
           />
         </div>
         <h2>Design Team</h2>
         <div className="team_con">
           <TeamCard
             link="https://www.linkedin.com/in/nikhil-tripathi/"
-            name="Nikhil Tripathi"
-            title="Assistant Professor, IIIT Sri City"
+            name="Shashank Raj"
+            title="Web Dev Lead, IOTA"
           />
           <TeamCard
-            name="Dr. Neha Agrawal"
+            name="Shashank Raj"
             link="https://www.linkedin.com/in/dr-neha-agrawal-0a32776b/"
-            title="Assistant Professor, IIIT Sri City"
+            title="Web Dev Lead, IOTA"
           />
           <TeamCard
             link="https://www.linkedin.com/in/nikhil-tripathi/"
-            name="Nikhil Tripathi"
-            title="Assistant Professor, IIIT Sri City"
+            name="Shashank Raj"
+            title="Web Dev Lead, IOTA"
           />
           <TeamCard
-            name="Dr. Neha Agrawal"
+            name="Shashank Raj"
             link="https://www.linkedin.com/in/dr-neha-agrawal-0a32776b/"
-            title="Assistant Professor, IIIT Sri City"
+            title="Web Dev Lead, IOTA"
           />
         </div>
       </div>
